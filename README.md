ae0143be4194f88e55433d79389e6cdebd78c73b
rishuinfinity <rishuinfinity@gmail.com>
2021-11-06 05:31:27 +0530

    Uploading all files

---
+++ README.md
@@ -0,0 +1,111 @@
+# ⇅ Internet Speed Monitor
+
+A Gnome extension to Monitor Internet Speed and Daily Data Usage in an elegant and minimal fashion.
+
+## Table of Contents
+
+* [Screenshot](#screenshot)
+* [Features](#features)
+* [Getting Started](#getting-started)
+  + [Prerequisite:](#prerequisite-)
+    - [Install Gnome Tweaks](#install-gnome-tweaks)
+  + [Install Internet Speed Monitor from Gnome Extensions](#install-internet-speed-monitor-from-gnome-extensions)
+  + [Install Internet Speed Monitor from source](#install-internet-speed-monitor-from-source)
+* [Contributing](#contributing)
+* [Thanks to](#thanks-to)
+* [License](#license)
+
+
+## Screenshot
+
+![Screenshot](screenshot.png)
+
+## Features
+
+This extension has following features:
+
+- Shows internet upload speed and download speed minimally
+- Shows daily data usage since midnight
+- Position of the extension is set on left hand side
+- Upload and Download speeds are shown after '↑' and '↓' respectively.
+- Speed is shown with just one letter
+   + K means KB/s
+   + M means MB/s
+   + G means GB/s
+   + T means TB/s
+- Similarly, data used is also shown with one letter after '=' symbol.
+
+## Getting Started
+
+To use this extension, you will need
+
+- Gnome Shell 3.20 or later
+
+### Prerequisite: Install Gnome Tweaks
+
+For Ubuntu,
+
+```bash
+sudo add-apt-repository universe
+sudo apt install gnome-tweaks
+```
+
+For Fedora,
+
+```bash
+sudo dnf install gnome-tweaks
+```
+
+For Arch Linux,
+
+```bash
+sudo pacman -S gnome-tweak-tool
+```
+
+You may find Gnome Tweaks for other distributions as well. Install them accordingly.
+
+### Install Internet Speed Monitor from Gnome Extensions
+
+Visit [Internet Speed Monitor - GNOME Shell Extensions](https://extensions.gnome.org/extension/) 
+
+### Install Internet Speed Monitor from source
+
+1. Clone this repository
+
+   ```bash
+   git clone https://github.com/rishuinfinity/InternetSpeedMonitor.git
+   ```
+
+2. Change current directory to repository
+
+   ```bash
+   cd InternetSpeedMonitor
+   ```
+
+3. Now run
+
+   ```bash
+   chmod +x ./install.sh && ./install.sh
+   ```
+
+4. Hit `ALT + F2`
+
+5. Type `r` and hit `Enter`
+
+6. Open Gnome Tweaks 
+
+7. Enable Internet Speed Monitor extension in Gnome Tweaks
+
+## Contributing
+
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## Thanks to
+
+- This project is modified from [Internet Speed Meter](https://github.com/AlShakib/InternetSpeedMeter) by [Al Shakib](https://alshakib.dev) 
+
+## License
+
+[GNU General Public License v3.0](LICENSE)
+
+Copyright © 2021 - 2022 [Rishu Raj](https://github.com/rishuinfinity)
---
+++ src/extension.js
@@ -0,0 +1,170 @@
+/*
+* Name: Internet Speed Meter
+* Description: A simple and minimal internet speed meter extension for Gnome Shell.
+* Author: Rishu
+*/
+
+const St = imports.gi.St;
+const Main = imports.ui.main;
+const Gio = imports.gi.Gio;
+const GLib  = imports.gi.GLib;
+const Clutter = imports.gi.Clutter;
+const Mainloop = imports.mainloop;
+
+const refreshTime = 1.0; // Set refresh time to one second.
+const unitBase = 1024.0; // 1 GB == 1024MB or 1MB == 1024KB etc.
+const units = ["K", "M", "G", "T"];
+/////////////////////////////////////////
+let dataused = 0;
+let lastdataused = 0;
+let line2 = '';
+
+let prevUploadBytes = 0, prevDownloadBytes = 0;
+let uploadSpeed = 0.0, downloadSpeed = 0.0;
+let container, timeout, netSpeed, defaultNetSpeedText;
+let home_dir = GLib.get_home_dir();
+let logSize = 8000; // about 8k
+
+
+
+
+function getNetSpeed() {
+  try {
+    let file = Gio.file_new_for_path('/proc/net/dev');
+    let fileStream = file.read(null);
+    let dataStream = Gio.DataInputStream.new(fileStream);
+    let uploadBytes = 0;
+    let downloadBytes = 0;
+    let line = '';
+    while((line = dataStream.read_line(null)) != null) {
+      line = String(line);
+      line = line.trim();
+      let column = line.split(/\W+/);
+      if (column.length <= 2) break;
+      if (column[0] != 'lo' &&
+         !isNaN(parseInt(column[1])) &&
+         !column[0].match(/^br[0-9]+/) &&
+         !column[0].match(/^tun[0-9]+/) &&
+         !column[0].match(/^tap[0-9]+/) &&
+         !column[0].match(/^vnet[0-9]+/) &&
+         !column[0].match(/^virbr[0-9]+/)) {
+        uploadBytes = uploadBytes + parseInt(column[9]);
+        downloadBytes = downloadBytes + parseInt(column[1]);
+      }
+    }
+    fileStream.close(null);
+    dataStream.close(null);
+
+    ////////////////////////// Read last data used bytes
+    let file2 = Gio.file_new_for_path(home_dir +'/.local/share/gnome-shell/extensions/InternetSpeedMonitor@Rishu/last');
+    let fileStream2 = file2.read(null);
+    let dataStream2 = Gio.DataInputStream.new(fileStream2);
+    line2 = dataStream2.read_line(null)
+    line2 = String(line2);
+    // line2 = line2.split(/\W+/);
+    line2 = line2.split(" ");
+    lastdate = line2[1];
+    line2 = line2[0];
+    lastdataused = line2;
+    
+
+    // file2.replace( null,false, 0, null ).close(null);
+    // let dataStream = file2.append_to( 1, null );
+    // dataOutStream.write( "e", null );
+    // dataOutStream.close(null);
+
+    fileStream2.close(null);
+    dataStream2.close(null);
+
+    date = new Date().toLocaleDateString()
+    if(date != lastdate){
+      resetLastData(date);
+    }
+    //////////////////////////////////////
+    if (prevUploadBytes === 0) {
+      prevUploadBytes = uploadBytes;
+    }
+    if (prevDownloadBytes === 0) {
+      prevDownloadBytes = downloadBytes;
+    }
+    // Current upload speed
+    uploadSpeed = (uploadBytes - prevUploadBytes) / (refreshTime * unitBase);
+
+    // Current download speed
+    downloadSpeed = (downloadBytes - prevDownloadBytes) / (refreshTime * unitBase);
+
+    // Total internet used
+    dataused = (uploadBytes + downloadBytes) / unitBase
+;    
+    // Show upload + download = total speed on shell
+    netSpeed.set_text("↑ " + netSpeedFormat(uploadSpeed) + " ↓ " + netSpeedFormat(downloadSpeed) + " = " + netSpeedFormat(dataused - lastdataused));
+    prevUploadBytes = uploadBytes;
+    prevDownloadBytes = downloadBytes;
+  } catch(e) {
+    netSpeed.set_text( defaultNetSpeedText  + " " + e);
+    saveExceptionLog(e);
+  }
+  return true;
+}
+
+function netSpeedFormat(speed) {
+  let i = 0;
+  while(speed >= unitBase) {  // Convert speed to KB, MB, GB or TB
+    speed /= unitBase;
+    i++;
+  }
+  return String(speed.toFixed(2) + "" + units[i]);
+}
+
+function saveExceptionLog(e){
+  let log_file = Gio.file_new_for_path( 
+    home_dir + '/.local/var/log/InternetSpeedMonitor.log' );
+
+  let log_file_size =  log_file.query_info( 
+    'standard::size', 0, null).get_size();
+  
+  if( log_file_size > logSize ){
+    log_file.replace( null,false, 0, null ).close(null);
+  }
+  e += Date()+':\n' + e;
+  let logOutStream = log_file.append_to( 1, null );
+  logOutStream.write( e, null );
+  logOutStream.close(null);
+
+}
+function resetLastData(d){
+  let file2 = Gio.file_new_for_path(home_dir +'/.local/share/gnome-shell/extensions/InternetSpeedMonitor\@Rishu/last');
+
+  file2.replace( null,false, 0, null ).close(null);
+  let dataOutStream = file2.append_to( 1, null );
+  dataOutStream.write( dataused.toString() + " " + d + " ", null );
+  dataOutStream.close(null);
+}
+
+function init() {
+  container = new St.Bin({
+    style_class: 'panel-button',
+    reactive: true,
+    can_focus: false,
+    x_expand: true,
+    y_expand: false,
+    track_hover: false
+  });
+  defaultNetSpeedText = '⇅ -.-- --';
+  netSpeed = new St.Label({
+    text: defaultNetSpeedText ,
+    style_class: 'netSpeedLabel',
+    y_align: Clutter.ActorAlign.CENTER
+  });
+  container.set_child(netSpeed);
+}
+
+function enable() {
+  Main.panel._leftBox.insert_child_at_index(container, 20);
+  timeout = Mainloop.timeout_add_seconds(refreshTime, getNetSpeed);
+}
+
+function disable() {
+  Mainloop.source_remove(timeout);
+  Main.panel._leftBox.remove_child(container);
+}
